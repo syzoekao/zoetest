a4f27b428de05579b0fab87db14231e72aba93fc
Zoe Kao <syzoekao@gmail.com>
2021-10-21 22:05:22 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+    name='zoetest',
+    version='1.0',
+    description='Testing install package on GitHub',
+    author='Zoe Cow',
+    # maintainer_email='pablo@pablohoffman.com',
+    # packages=find_packages(exclude=('tests', 'tests.*')),
+    # zip_safe=False,
+    # entry_points={
+    #     'console_scripts': ['scrapy = scrapy.cmdline:execute']
+    # },
+    # classifiers=[
+    #     'Framework :: Scrapy',
+    #     'Development Status :: 5 - Production/Stable',
+    #     'Environment :: Console',
+    #     'Intended Audience :: Developers',
+    #     'License :: OSI Approved :: BSD License',
+    #     'Operating System :: OS Independent',
+    #     'Programming Language :: Python',
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.6',
+    #     'Programming Language :: Python :: 3.7',
+    #     'Programming Language :: Python :: 3.8',
+    #     'Programming Language :: Python :: 3.9',
+    #     'Programming Language :: Python :: 3.10',
+    #     'Programming Language :: Python :: Implementation :: CPython',
+    #     'Programming Language :: Python :: Implementation :: PyPy',
+    #     'Topic :: Internet :: WWW/HTTP',
+    #     'Topic :: Software Development :: Libraries :: Application Frameworks',
+    #     'Topic :: Software Development :: Libraries :: Python Modules',
+    # ],
+    python_requires='>=3.9',
+)
---
+++ zoetest/zoetest.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def mysample(a, b):
+    return np.random.choice(a, b)

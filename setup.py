74a05f966272068840b4aeb9280e3470ffe52681
Zoe Kao <syzoekao@gmail.com>
2021-10-24 14:21:35 -0400

    updates

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     name='zoetest',
     version='1.0',
     description='Testing install package on GitHub',
-    author='Zoe Cow',
-    install_requires=[# 'cython',
-                      'numpy>=1.18'],
+    author='Moo Moo the Cow',
+    install_requires=['numpy>=1.18'],
     python_requires='>=3.9',
 )
